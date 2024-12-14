c9f317ea7e8ee94a3b6cfd3dfecc335c34fa2cee
re_sistance <rostormstudios@gmail.com>
2024-12-14 11:49:59 -0500

    Update Shark.lua

--- Shark.lua
+++ Shark.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RoStormCreations/SHARKHUB/refs/heads/main/SupportedGames.lua"))()
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
         loadstring(game:HttpGet(Execute))()
