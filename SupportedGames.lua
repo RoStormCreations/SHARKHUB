de51dd8f52430b44d988df06a9ae391f38474cb2
re_sistance <rostormstudios@gmail.com>
2024-12-14 12:39:54 -0500

    Update SupportedGames.lua

--- SupportedGames.lua
+++ SupportedGames.lua
@@ -1,4 +1,4 @@
 getgenv().Games = {
-  [2753915549] = "", -- Blox Fruit Sea 1
-  [16732694052] = "" -- Fisch
+  [2753915549] = "https://raw.githubusercontent.com/RoStormCreations/SHARKHUB/refs/heads/main/games/001.lua", -- Blox Fruit Sea 1
+  [16732694052] = "https://raw.githubusercontent.com/RoStormCreations/SHARKHUB/refs/heads/main/games/001.lua" -- Fisch
 }
